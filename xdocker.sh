51b0b2bd47a142782ae534cf6040cfa5b51386cf
filomaq <68634756+filomaq@users.noreply.github.com>
2022-08-30 14:31:26 +0900

    Update xdocker.sh

--- xdocker.sh
+++ xdocker.sh
@@ -5,13 +5,17 @@ if [ -n "${XDOCKER_DISP_NUM}" ]; then
         XDOCKER_UNIX_FILE=$XDOCKER_UNIX_DIR/X$XDOCKER_DISP_NUM
         XDOCKER_DISP_PORT=$(expr $XDOCKER_DISP_NUM + 6000)
 
-        mkdir -p -m 777 $XDOCKER_UNIX_DIR
+        mkdir -p -m 777 "$XDOCKER_UNIX_DIR"
         shopt -s huponexit
         if [ ! -S "$XDOCKER_UNIX_FILE" ]; then
-                socat UNIX-LISTEN:$XDOCKER_UNIX_FILE,mode=777,fork TCP:localhost:$XDOCKER_DISP_PORT &
+                socat UNIX-LISTEN:"$XDOCKER_UNIX_FILE",mode=777,fork TCP:localhost:$XDOCKER_DISP_PORT &
         fi
 
         export XDOCKER_DISP=:$XDOCKER_DISP_NUM
         export XDOCKER_AUTH=$XDOCKER_DISP$(xauth list $DISPLAY | grep -o "\s\+.\+")
         export XDOCKER_UNIX=$XDOCKER_UNIX_DIR
+
+        export XDOCKER_HOSTNAME="localhost"
+        export XDOCKER_AUTHFILE=$(mktemp)
+        xauth -f "$XDOCKER_AUTHFILE" add $XDOCKER_HOSTNAME/unix$XDOCKER_AUTH
 fi
